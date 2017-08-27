7ea390a948d829ea2f35f9a448046f720f4af378
Marvin Arcilla <arcilla.bumblebeen@gmail.com>
2017-08-27 10:51:46 +0800

    Added async/await plugin

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 
 class Greeter {
 	constructor (message) {
@@ -7,9 +8,18 @@ class Greeter {
 	greet() {
 		console.log(this.message);
 	}
+
+	async fortuneGreeting() {
+		let fortuneMessage = await fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
+	      .then(res => res.json())
+	      .then(res => {
+	        return res[0].fortune.message;
+	      });
+	    console.log('Here is your timely fortune: ' + fortuneMessage);
+	}
 }
 
 let helloWorld = new Greeter('Hello World');
-helloWorld.greet();
+helloWorld.fortuneGreeting();
 
 export default Greeter;
